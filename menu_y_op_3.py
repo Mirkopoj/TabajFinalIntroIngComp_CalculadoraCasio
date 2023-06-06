3b69bf5cfe6f59397576b4aa409a36716eebe481
Mirkopoj <mirkopoj@hotmail.com>
2023-06-06 09:39:20 -0300

    conversiones en su propio archivo

--- menu_y_op_3.py
+++ menu_y_op_3.py
@@ -1,114 +1,9 @@
+import conversiones as con
+
 # considerar usar el clear para la calculadora
 calc_On = True
 expresion = 0
 boton = 1
-
-#--------------------- CONVERSIONES --------------------------------------------------
-def conversiones(expresion):
-    num = expresion
-    opcion = 0
-    resultado = 0
-    band = True
-
-    while band == True:
-        print("\n1- Binario")
-        print("2- Hexadecimal")
-        print("3- Octal")
-
-        opcion = int(input("\n_"))
-
-        if opcion == 1:
-            resultado = binario(num)
-            band = False
-        elif opcion == 2:
-            resultado = hexadecimal(num)
-            band = False
-        elif opcion == 3:
-            resultado = octal(num)
-            band = False
-        else:
-            print("Ingrese un numero valido")
-    return resultado
-
-#--------------------- BINARIO ---------------------------------------------------------
-def binario(num): 
-    actual = num
-    aux = 2
-    lista=[]
-
-    while aux > 1:
-        aux = actual / 2
-        lista.append(int(actual % 2))
-        actual = aux
-    lista = lista_reversa(lista) 
-    return lista
-#--------------------- HEXADECIMAL --------------------------------------------------
-def hexadecimal(num):
-    band_H = True
-    lista_H=[]
-    siguiente_num = 0
-    redondeo = 0
-    resto = 0 
-    inte = 0
-    multi = 0 
-
-    while band_H == True:
-        siguiente_num = num / 16
-        redondeo = int(siguiente_num)
-        multi= redondeo * 16 
-        resto = num - multi
-        inte = int(resto)
-        num = siguiente_num  
-        lista_H.append(inte)
-
-        if redondeo == 0:
-            band_H = False
-
-    i=0 
-    j=0
-    letras=['A','B','C','D','E','F']
-    for i in range(len(lista_H)):
-        for j in range(6): # 6 porque va de 0 a 5
-            if lista_H[i] == (j+10):
-                lista_H[i] = letras[j]
-        j=0 # resetea el for 
-    lista_H = lista_reversa(lista_H)
-    return lista_H
-#--------------------- OCTAL -------------------------------------------------------
-def octal(num):
-    band_o = True
-    resultado = [] #lista vacia
-    siguiente_num = 0
-    entero = 0
-    mult = 0
-    resto = 0
-    entero2 = 0
-
-    while band_o == True:
-
-        siguiente_num = num / 8
-        entero = int(siguiente_num)
-        mult = entero * 8
-        resto = num - mult
-        entero2 = int(resto)
-        num = siguiente_num
-        resultado.append(entero2)
-
-        if entero == 0:
-            band_o = False
-
-    resultado = lista_reversa(resultado)
-    return resultado
-
-#--------------------- LISTA_REVERSA -----------------------------------------------
-def lista_reversa(lista):
-    a = len(lista)
-    lista_R = []
-    for i in range(len(lista)):
-        a -= 1
-        b = lista[a]
-        lista_R.append(b)
-    return lista_R
 
 #--------------------- MENU -----------------------------------------------------------
 #                                    8b    d8  888888  88b 88  88   88 
@@ -142,7 +37,7 @@ while calc_On == True:
             print("\nIngrese un numero valido ")
             expresion = int(input("\n_"))
 
-        lista_convertido = conversiones(expresion)
+        lista_convertido = con.conversiones(expresion)
         print(f"Conversión: {lista_convertido} ")
 
     elif select == 4:
