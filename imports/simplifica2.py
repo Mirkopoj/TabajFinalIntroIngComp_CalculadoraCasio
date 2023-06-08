77a0d2dc121de81b0aac5576696a57182cc7d9f5
Mirko <mirkopoj@hotmail.com>
2023-06-08 14:28:50 -0300

    Simplifica bien

--- imports/simplifica2.py
+++ imports/simplifica2.py
@@ -5,8 +5,9 @@ if __name__ == "__main__":
 def simplify(frac: tuple[int,int]) -> tuple[int,int]:
     retnum = frac[0]
     retden = frac[1]
-    for i in range(frac[1],2,-1):
+    for i in range(frac[1],1,-1):
         if retden%i==0 and retnum%i==0:
             retnum /= i
             retden /= i
+            i = i+1
     return (int(retnum), int(retden))
