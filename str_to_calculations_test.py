9c65e966d781d48f0e2294d578a0cf9b11632d57
Mirko <mirkopoj@hotmail.com>
2023-06-08 21:12:19 -0300

    Tests caja blanca parser

--- str_to_calculations_test.py
+++ str_to_calculations_test.py
@@ -1,5 +1,6 @@
 from imports import str_to_calculations as stc
 import unittest
+from imports import funciones as fn
 
 class TestStrToCalc(unittest.TestCase):
 
@@ -34,6 +35,74 @@ class TestStrToCalc(unittest.TestCase):
     def test_blanca_tokenizer_8(self):
         res = stc.tokenize("abcd")
         self.assertEqual(res, ["abcd"])
+
+    def test_blanca_parser_11(self):
+        res = stc.parser(['1'])
+        self.assertEqual(res, [1])
+
+    def test_blanca_parser_3(self):
+        res = stc.parser([''])
+        self.assertEqual(res, [])
+
+    def test_blanca_parser_1_2_11_16(self):
+        res = stc.parser(['1','|','2'], fn.hacer_fraccion)
+        self.assertEqual(res, [(1,2)])
+
+    def test_blanca_parser_1_2_11_15(self):
+        res = stc.parser(['1','|','2', '3'], fn.hacer_fraccion)
+        self.assertEqual(res, [(1,2),3])
+
+    def test_blanca_parser_11_12(self):
+        crash =False
+        try:
+            stc.parser(['1','|','2'])
+        except:
+            crash = True
+        self.assertTrue(crash)
+
+    def test_blanca_parser_13(self):
+        res = stc.parser(['|'], fn.hacer_fraccion)
+        self.assertEqual(res, ['|'])
+
+    def test_blanca_parser_14_3(self):
+        res = stc.parser(['|', ''], fn.hacer_fraccion)
+        self.assertEqual(res, ['|'])
+
+    def test_blanca_parser_8(self):
+        crash =False
+        try:
+            stc.parser(['+'])
+        except:
+            crash = True
+        self.assertTrue(crash)
+
+    def test_blanca_parser_4_11(self):
+        res = stc.parser(['1','+'])
+        self.assertEqual(res, [1,'+'])
+
+    def test_blanca_parser_5(self):
+        res = stc.parser(['1','-'])
+        self.assertEqual(res, [1,'-'])
+
+    def test_blanca_parser_6(self):
+        res = stc.parser(['1','*'])
+        self.assertEqual(res, [1,'*'])
+
+    def test_blanca_parser_7(self):
+        res = stc.parser(['1','/'])
+        self.assertEqual(res, [1,'/'])
+
+    def test_blanca_parser_9(self):
+        crash =False
+        try:
+            stc.parser(['='])
+        except:
+            crash = True
+        self.assertTrue(crash)
+
+    def test_blanca_parser_10(self):
+        res = stc.parser(['1','='])
+        self.assertEqual(res, [1,'=',' '])
 
 if __name__ == "__main__":
     unittest.main()
